96436878403a37d463f8ab210432a169d273a977
Vigo28 <gamingvigo06@gmail.com>
2025-06-06 10:15:28 +0300

    Fix all foreign problems

--- database/migrations/2025_06_06_055612_add_user_id_to_task_table.php
+++ database/migrations/2025_06_06_055612_add_user_id_to_task_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign(["user_id"]);
             $table->dropColoumn("user_id");
         });
     }
--- database/migrations/2025_06_06_070836_add_task_status_id_to_task_table.php
+++ database/migrations/2025_06_06_070836_add_task_status_id_to_task_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropForeign(["task_statuses"]);
+            $table->dropForeign(["task_status_id"]);
             $table->dropcolumn(["task_status_id"]);
         });
     }
