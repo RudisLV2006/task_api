b1a561d224332a8427b0412adf22aa7bc5b7448d
Vigo28 <gamingvigo06@gmail.com>
2025-06-06 10:10:06 +0300

    Add taskstatus model and migration, add task_status_id to task

---
+++ database/migrations/2025_06_06_070836_add_task_status_id_to_task_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignId('task_status_id')->default(1)->constrained('task_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign(["task_statuses"]);
+            $table->dropcolumn(["task_status_id"]);
+        });
+    }
+};
