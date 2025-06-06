e1c019db3049e1df18ba3a97e8abc08a6a71d8e4
Vigo28 <gamingvigo06@gmail.com>
2025-06-06 09:57:01 +0300

    Subtask controller add middleware to check what user try add

--- app/Http/Controllers/SubtaskController.php
+++ app/Http/Controllers/SubtaskController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class SubtaskController extends Controller
+class SubtaskController extends Controller implements HasMiddleware
 {
+    public static function middleware(){
+        return [
+            new Middleware("auth:sanctum")
+        ];
+    } 
     public function store(Request $request, Task $task){
+        Gate::authorize('modify',$task);
         $fields = $request->validate([
             "content"=>"required"
         ]);
